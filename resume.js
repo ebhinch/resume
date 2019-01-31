2c8a1bcc55061923c6fb21ee388101a8f0221bd1
ebhinch <erica.hinchman@gmail.com>
2019-01-31 13:33:52 -0500

    Filled out resume file

---
+++ resume.js
@@ -0,0 +1,173 @@
+{
+    firstName: "Erica",
+    lastName: "Hinchman",
+    email: "erica@ericahinchman.com",
+    phone: "404.234.3528",
+    city: "Atlanta",
+    countryCode: "+1",
+    linkedInURL: "https://linkedin.com/in/erica-hinchman/",
+    githubUrl: "https://github.com/ebhinch",
+    workExperience: [
+        {
+            fromDate: "March 12, 2018 - 9 a.m.",
+            toDate: "December 12, 2018 - 5 p.m.",
+            responsibilities: [
+                {
+                "Developed production features for 2018 CNN Election Center app using ReactJS, JSX, CSS, HTML: www.cnn.com/election/2018/exit-polls",
+                "Full-scale manual testing for 2018 Primaries and General Midterm elections applications",
+                "Logged and managed all bugs within application",
+                "Participation in election rehearsals to evaluate behavior of applications when receiving live data",
+                "Collaborated on cross-functional Agile team, participating in daily stand up, bi-weekly sprint planning and story pointing, and retros following each sprint."
+                }
+        ],
+            description: "I worked in a hybrid front-end development and manual QA role on CNN Digital's Politics team, building and testing the 2018 midterm primaries and general election center applications. The 2018 election cycle was the first time CNN Digital has covered midterm elections and the first time CNN Digital has had a dedicated Politics team. Working on a collaborative, cross-functional, Agile team, allowed me to hone a very granular analysis technique, often anticipating issues before they arose - something that only furthered my development approach. I contributed to the production code base, building out dynamic new features that enhance user experience on the 2018 CNN Election Center. Working directly as part of the dev team, and putting my coding skills to work in a professional setting solidified that development is where my passion lies.",
+            title: "Junior QA Analyst",
+            company: "CNN"
+    },
+    {
+        fromDate: "April 11, 2016 - 8 a.m.",
+        toDate: "April 12, 2017 - 5 p.m.",
+        responsibilities: [
+            "Managed public relations efforts both Children's Healthcare of Atlanta's Strong4Life external wellness program and internal employee and physician wellness programs",
+            "Worked closely with local and national media to secure print, digital, and broadcast press placements",
+            "Exceed 2016 media goal, more than doubling the ROI",
+            "Drafted and distributed media pitches and press releases on behalf of CHOA's Strong4Life",
+            "Coordinated and provided on-site management for annual Superhero Sprint fundraiser",
+            "Collaborated closely with Strong4Life medical, research, and wellness staff to identify story angles, upcoming studies, and research developments for media outreach."
+        ],
+        description: "Worked as only public relations employee on Children's Healthcare of Atlanta's Strong4Life communications and marketing team. Also collaborated with larger hospital communications teams on larger hospital-wide projects.",
+        title: "Public Relations Coordinator",
+        company: "Children's Healthcare of Atlanta"
+    },
+    {
+        fromDate: "January 1, 2016 - 10 a.m.",
+        toDate: "April 1, 2016 - 5 p.m.",
+        responsibilities: [
+            "Managed PR and marketing for account base of local and regional hospitality and lifestyle companies.",
+            "Led public relations efforts, distributing media pitches and press releases, for account base, securing placements in local, regional, and national print, digital, and broadcast media.",
+            "Coordinated and executed client events ranging from restaurant openings and month-long festivals to targeted media dinners",
+            "Worked as PR team editor, reviewing and distributing communications on behalf of the company, including press releases and weekly newsletters"
+        ],
+        description: "I worked in restaurant, hospitality, and lifestyle public relations and marketing for a small agency. Our client base included in local and regional restaurants, national hotel brands, local and regional culinary and lifestyle events, and more. While at the company, which started as The Reynolds Group and was then acquired by Phase 3, I worked as a PR intern, account coordinator, senior account coordinator, and account manager.",
+        title: "Public Relations Manager",
+        company: "Phase 3 Marketing and Communications (which acquired The Reynolds Group in late 2015."    
+    },
+    {
+        fromDate: "October 1, 2012",
+        toDate: "December 31, 2015",
+        responsibilities: [
+            "Managed PR and marketing efforts for account base of local and regional lifestyle and hospitality clients",
+            "Assisted in planning client events, including media events and restaurant openings",
+            "Drafted and distributed client press releases and pitches",
+            "Drafted and contributed copy to weekly company newsletter",
+            "Led training and onboarding for new team members both junior and senior to me"
+        ],
+        description: "I worked in restaurant, hospitality, and lifestyle public relations and marketing for a boutique agency based in Atlanta, though I also worked in the Charleston, S.C., office for one year.",
+        title: "Account Manager",
+        company: "The Reynolds Group (later acquired by Phase 3 Marketing and Communications)"
+    },
+    {
+        fromDate: "February 1, 2012",
+        toDate: "August 1, 2012",
+        responsibilities: [
+            "Led coffee and tea service",
+            "Managed orders placed in restaurant as well as large-scale catering orders"
+        ],
+        description: "I worked in a fast-paced cafe in Charlottesville, Virginia, assisting with everything from day-to-day order taking, and catering order placement, to managing coffee and tea service.",
+        title: "Lead Shop Assistant and Barista",
+        company: "Hotcakes"
+    },
+    {
+        fromDate: "July 1, 2010",
+        toDate: "November 15, 2011",
+        responsibilities: [
+            "Led food runner service in fine-dining steakhouse",
+            "Trained new employees",
+            "Worked as hostess, managing reservations, private event bookings, and more."
+        ],
+        description: "I worked as a food runner and hostess at Charlottesville, Virginia's most acclaimed steakhouse.",
+        title: "Food Runner and Hostess",
+        companies: "The Downtown Grille"
+    },
+    {
+        fromdate: "December 1, 2010",
+        toDate: "June 15, 2011",
+        responsibilities: [
+            "Conducted primary-resource research for director of University of Virginia's Miller Center's then in-progress book, a historical review of the agricultural past of Louisa County, Virginia.",
+            "Prioritized relevant resources and collaborated with fellow interns to determine research worth sharing with professor Brian Balogh."
+        ],
+        description: "I worked as a research intern for the director of the University of Virginia Miller Center of Public Affairs. I conducted public history research examining rural Virginia and the effect its rural past had on its modern identity.",
+        title: "Research Intern",
+        company: "The Miller Center"
+    }
+    ],
+    skills: [
+        "JavaScript",
+        "ReactJS",
+        "Agile Project Management",
+        "JSX",
+        "CSS",
+        "HTML",
+        "NodeJS",
+        "git",
+        "GitHub",
+        "Styled components",
+        "npm",
+        "yarn",
+        "Jira",
+        "TestRail",
+        "BrowserStack",
+        "Express",
+        "Postman",
+        "Ruby on Rails",
+        "React on Rails",
+        "pair programming"
+    ],
+    education: [
+        {
+            establishment: "General Assembly",
+            degree: "Certificate from Web Development Immersive boot camp",
+            city: "Atlanta",
+            countryCode: "+1",
+            fromYear: "2017",
+            toYear: "2017"
+        },
+        {
+            establishment: "University of Virginia",
+            degree: "Bachelor of Arts in History",
+            city: "Charlottesville, Va.",
+            countryCode: "+1",
+            fromYear: "2007",
+            toYear: "2010"
+        },
+        {
+            establishment: "Southern Methodist University",
+            degree: "n/a",
+            city: "Dallas",
+            countryCode: "+`",
+            fromYear: "2006",
+            toYear: "2007"
+        },
+        {
+            establishment: "The Westminster Schools",
+            degree: "High School Diploma",
+            city: "Atlanta",
+            countryCode: "+1",
+            fromYear: "1993",
+            toYear: "2006"
+        }
+    ],
+    languages: {
+        "English"
+    },
+    hobbies: {
+        "Exploring local farmers’ markets",
+        "Experimenting in my kitchen",
+        "Cooking for friends and family",
+        "FlyWheel",
+        "Yoga",
+        "Spending as much time outside as possible",
+        "Taking my dog, Olive, to the park"
+    }
+}
+
